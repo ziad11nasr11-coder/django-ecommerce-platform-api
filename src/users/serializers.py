bb156dd21027d391792bc24b45ad392c449136c2
ziad <ziad11nasr11@gmail.com>
2026-04-09 18:47:49 +0200

    Add user authentication serializers (register, login, password reset)

---
+++ src/users/serializers.py
@@ -0,0 +1,61 @@
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from users.models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    user_type = serializers.ChoiceField(
+        choices=[
+            ('seller', 'Seller'),
+            ('customer', 'Customer'),
+        ]
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'phone', 'password', 'user_type']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(
+            username=data['username'],
+            password=data['password']
+        )
+
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+
+        return user
+
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(write_only=True)
+    new_password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = self.context['request'].user
+
+        if not user.check_password(data['old_password']):
+            raise serializers.ValidationError("Old password is incorrect")
+
+        return data
+
+    def save(self, **kwargs):
+        user = self.context['request'].user
+        user.set_password(self.validated_data['new_password'])
+        user.save()
+
+class ForgotPasswordSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+class ResetPasswordSerializer(serializers.Serializer):
+    token = serializers.CharField()
+    new_password = serializers.CharField(write_only=True)
+
